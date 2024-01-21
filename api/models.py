70db64d4eee4e758bf4bd8e4986c2fa9d3a5cca0
sodiqdevpython <sodiqdevpython@gmail.com>
2024-01-21 14:29:01 +0500

    added some new features

--- api/models.py
+++ api/models.py
@@ -4,17 +4,19 @@ class MainData(models.Model):
     tg_id = models.PositiveBigIntegerField(unique=True) # Telegram id
     tel_number = models.CharField(max_length=20) # Telefon raqam
     fio = models.CharField(max_length=100) # Ism, familiya, otasining ismi
-    new_square = models.FloatField() # Novie kvadrat
-    section = models.PositiveIntegerField() # Podiezd
-    floor = models.PositiveIntegerField() # Etaj
-    overall_sum = models.FloatField() # Jami summa
-    paid = models.FloatField() # To'langan
+    new_square = models.CharField(max_length=20) # Novie kvadrat
+    section = models.CharField(max_length=20) # Podiezd
+    floor = models.CharField(max_length=20) # Etaj
+    overall_sum = models.CharField(max_length=20) # Jami summa
+    paid = models.CharField(max_length=20) # To'langan
+    doc_number = models.CharField(max_length=10, unique=True) # document nomeri
+    doc_image = models.ImageField(upload_to='media') # document rasmi
     # posted_time = models.DateTimeField(auto_now_add = True) # Ushbu ma'lumot qo'yilgan vaqti
     # edited_time = models.DateTimeField(auto_now = True) # Edit qilingan vaqti
 
     def __int__(self):
         return self.tg_id
-    
+
     class Meta:
         verbose_name = "Ma'lumot"
         verbose_name_plural = "Ma'lumotlar"
--- run_api.py
+++ run_api.py
@@ -1,21 +1,20 @@
 import requests
-import json
 
-data = requests.get('http://127.0.0.1:8000')
-data = json.loads(data.text)
-print(data)
 post_data = {
-    "tg_id": 1231456,  
-    "tel_number": "123-456-7890",
-    "fio": "John Doe",
-    "new_square": 50.0,
-    "section": 1,
-    "floor": 3,
-    "overall_sum": 500.0,
-    "paid": 200.0
+    "tg_id": 12121,
+    "tel_number": "1212121",
+    "fio": "Oydinov S",
+    "new_square": "122112",
+    "section": "1212",
+    "floor": "1212",
+    "overall_sum": "121221",
+    "paid": "2112121",
+    "doc_number": "256",
 }
 
-# Make the POST request
-response = requests.post('http://127.0.0.1:8000/add/', data=post_data)
+# Use 'files' parameter to send files with requests.post
+files = {'doc_image': ('photo.jpg', open(r'C:\photo_2024-01-15_05-37-16.jpg', 'rb'), 'image/jpeg')}
 
-print(data)
+response = requests.post('http://127.0.0.1:8000/add/', data=post_data, files=files)
+
+print(response.text)
